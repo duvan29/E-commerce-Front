18a68925842dfa49d5cfcc7867a838540b49bf75
Duvan <duvanserrano21@gmail.com>
2023-08-02 19:05:31 -0500

    Styled

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import { styled } from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: #222;
+`;
 
 
 export default function Header(){
     return(
-        <header>
+        <StyledHeader>
             <Link href="/">Ecommerce</Link>
             <nav>
                 <Link  href="/">Home</Link>
@@ -12,6 +17,6 @@ export default function Header(){
                 <Link href="/account">Account</Link>
                 <Link href="/cart">Cart (0)</Link>
             </nav>
-        </header>
+        </StyledHeader>
     )
 }
